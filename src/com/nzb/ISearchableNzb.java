23f9b7192c19f4528615b3b6208f9121751375fa
hellhand <hellhand@c60aeec9-8a83-4df2-9222-f478e35f9233>
2012-06-20 11:05:18 +0000

    - Correction of multiple bugs. - Added server settings screen.
    git-svn-id: svn+ssh://svn.code.sf.net/p/sabdroidex/code/SABDroidEx@45 c60aeec9-8a83-4df2-9222-f478e35f9233

--- src/com/nzb/ISearchableNzb.java
+++ src/com/nzb/ISearchableNzb.java
@@ -5,8 +5,7 @@ public abstract class ISearchableNzb {
     /**
      * 
      * @param search The Search {@link String}
-     * @return a {@link String[][]} containing the result of the research. The inner array contains : 1 Name, 2 Size, 3 Category, 4 Password, 5 Image.
-     * 
+     * @return a {@link String[][]} containing the result of the research. The inner array contains : 1 Name, 2 Size, 3 description, 4 Category, 5 Password, 6 Image.
      */
     public abstract String[][] search(String search);
 }
